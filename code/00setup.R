6dd6ab9c8beebb6cf3de54043b95ee7203ccf1ce
Shuxin Dong <shuxind11@gmail.com>
2020-12-03 21:06:07 -0500

    Update 00setup.R

--- code/00setup.R
+++ code/00setup.R
@@ -2,8 +2,7 @@
 # Project: Medicare Mortality and Air Pollution in AD/ADRD                    #
 # Code: clean/merge datasets                                                  #
 # Input: "national_exp.fst", "hospital_total.rds"                             #
-# Output: "populationID.csv" as study population's IDs                        #
-# Output: "enrollyrINFO.csv" as the year of enrollment for each ID            #
+# Output: "enrolledINFO.csv" - IDs and  the year of enrollment                #
 # Output: "ADRDnational_exp.csv" as retricted "national_exp.fst"              #
 # Output: "ADRDmort_all.csv" as data added death info but not checking dups   #
 # Author: Shuxin Dong                                                         #
@@ -34,40 +33,44 @@ ADRDmed <- subset(med, Alzheimer_pdx==1|Alzheimer_pdx2dx_10==1|Alzheimer_pdx2dx_
                   select = c("QID", "ADATE"))
 ADRDmed$year_admit <- as.numeric(format(ADRDmed$ADATE, "%Y"))
 
-## save all the IDs with ADRD diagnoses
-id <- ADRDmed[!duplicated(QID),]
-id$include <- 1 # add a column to mark whether to include
-id[, year := NULL]
-head(id)
-fwrite(id, paste0(dir_output, "populationID.csv"))
+# ## save all the IDs with ADRD diagnoses
+# id <- ADRDmed[!duplicated(QID),]
+# id$include <- 1 # add a column to mark whether to include
+# id[, year := NULL]
+# head(id)
+# fwrite(id, paste0(dir_output, "populationID.csv"))
+# 
+# ## save enroll info for ADRD cohort
+# enrollyr <- aggregate(year_admit ~ QID, ADRDmed, min)
+# setnames(enrollyr, "year_admit", "firstADRDyear")
+# fwrite(enrollyr, paste0(dir_output, "enrollyrINFO.csv"))
 
-## save enroll info for ADRD cohort
-enrollyr <- aggregate(year_admit ~ QID, ADRDmed, min)
-setnames(enrollyr, "year_admit", "firstADRDyear")
-fwrite(enrollyr, paste0(dir_output, "enrollyrINFO.csv"))
+## drop subjects whose firstADRDyr <= 2006
+enrollINFO <- aggregate(year_admit ~ QID, ADRDmed, min)
+setnames(enrollINFO, "year_admit", "firstADRDyr")
+enrolledINFO <- enrollINFO[firstADRDyr>2006]
+fwrite(enrolledINFO, paste0(dir_output, "enrolledINFO.csv"))
 
 ##################### 2. load denominator file ################################
 # denom_file <- read_fst(paste0(dir_data,"national_exp.fst"))
 # >  dim(denom_file)
 # [1] 538173801        37
 # n.denom <- 538173801
-# id  <- fread(paste0(dir_output, "populationID.csv"))
+
+enrolledINFO  <- fread(paste0(dir_output, "enrolledINFO.csv"))
 
 ## split the national denominator file to save memory
 ## subset the dataset to those in ADRD cohort
 denom_file.1 <- read_fst(paste0(dir_data,"national_exp.fst"),
-                         from = 1, to = floor(n.denom/2))
-setDT(denom_file.1)
-ADRDdenom.1 <- denom_file.1[QID %in% id[,QID]]
+                         from = 1, to = floor(n.denom/2), as.data.table = TRUE)
+ADRDdenom.1 <- denom_file.1[QID %in% enrolledINFO[,QID]]
 rm(denom_file.1)
 gc()
 denom_file.2 <- read_fst(paste0(dir_data,"national_exp.fst"),
-                         from = ceiling(n.denom/2))
-setDT(denom_file.2)
-ADRDdenom.2 <- denom_file.2[QID %in% id[,QID]]
+                         from = ceiling(n.denom/2), as.data.table = TRUE)
+ADRDdenom.2 <- denom_file.2[QID %in% enrolledINFO[,QID]]
 rm(denom_file.2)
 gc()
-
 ADRDdenom <- rbind(ADRDdenom.1, ADRDdenom.2) # combine splits
 rm(ADRDdenom.1, ADRDdenom.2)
 gc()
@@ -77,20 +80,20 @@ fwrite(ADRDdenom, paste0(dir_output, "ADRDnational_exp.csv"))
 gc()
 
 ADRDdenom <- fread(paste0(dir_output, "ADRDnational_exp.csv"))
-enrollyr <- fread(paste0(dir_output, "enrollyrINFO.csv"))
 
 ## add "firstADRDyear"
-ADRDdenom <- merge(ADRDdenom, enrollyr, by="QID")
-table(ADRDdenom$firstADRDyear)
+ADRDdenom <- merge(ADRDdenom, enrolledINFO, by="QID")
+table(ADRDdenom$firstADRDyr)
 
 ## start to follow-up after firstADRDyear
 ADRDmort <- ADRDdenom[year_admit>=firstADRDyear]
 
 ## get death info as mortINFO
 ADRDmort$bene_dod <- as.Date(ADRDmort$bene_dod, format = "%Y-%m-%d") # convert format
-mortINFO <- ADRDmort[,.(QID, bene_dod)][!is.na(bene_dod)][, mort_yr := as.numeric(format(bene_dod, "%Y"))] #get death year for each ID
-# mortINFO[!duplicated(mortINFO)][duplicated(QID)]
-mortINFO <- mortINFO[!duplicated(mortINFO)][, ':=' (bene_dod = NULL, death = 1)]
+mortINFO <- ADRDmort[,.(QID, bene_dod)][!is.na(bene_dod)]
+mortINFO[, mort_yr := as.numeric(format(bene_dod, "%Y"))] #get death year for each ID
+mortINFO[!duplicated(mortINFO)][duplicated(QID)]
+mortINFO[!duplicated(mortINFO)][, ':=' (bene_dod = NULL, death = 1)][]
 
 ## add "mort_year" and "death" status into ADRDmort
 ADRDmort <- merge(ADRDmort, mortINFO, by = "QID", all.x = TRUE)
@@ -100,21 +103,19 @@ summary(ADRDmort$death)
 ## Drop years after death
 ADRDmort <- subset(ADRDmort, death==0|(death==1 & year_admit <= mort_yr))
 fwrite(ADRDmort, paste0(dir_output, "ADRDmort_all.csv"))
-
-ADRDmort <- fread(paste0(dir_output, "ADRDmort_all.csv"))
 ##################### 3. check completeness of follow-up ######################
+ADRDmort <- fread(paste0(dir_output, "ADRDmort_all.csv"))
+
 ## drop info of admission dates, diagnoses, diabetes
-ADRDmort[, ':=' (bene_dod=NULL, 
-                 ADATE=NULL,DDATE=NULL, year=NULL,
-                 DIAG1=NULL, DIAG2=NULL,
-                 diabetes=NULL)]
+check <- ADRDmort[, !c("bene_dod", "ADATE", "DDATE", "year", "DIAG1", "DIAG2"), with=FALSE]
 
 ## remove duplication
-ADRDmort <- ADRDmort[!duplicated(ADRDmort)]
+check <- check[!duplicated(ADRDmort)]
 
 ## detect follow-up info problems
-temp <- ADRDmort[,.(QID, year_admit, firstADRDyear, mort_yr, death)][,followyr := year_admit-firstADRDyear]
-temp[,.(min.followyr = min(followyr)), by=QID][,min.followyr] %>% table() #detect problems
+temp <- check[,.(QID, year_admit, firstADRDyear, mort_yr, death)]
+temp[,followyr := year_admit-firstADRDyear]
+temp[,.(min.followyr = min(followyr)), by=QID][, min.followyr] %>% table() #detect problems
 
 ## for those alive 
 alive <- temp[death==0]
@@ -122,5 +123,4 @@ ideal_alive <- merge(alive[,.(max.year_admit = max(year_admit)), by = QID], enro
 
 temp[deaht==0][,.(max.year_admit = max(year_admit)), by = QID]
 
-merge(temp[,.(max.year_admit = max(year_admit)), by = QID], enrollyr, by = "QID")
-[max.year_admit == mort_yr]
+merge(temp[,.(max.year_admit = max(year_admit)), by = QID], enrollyr, by = "QID")[max.year_admit == mort_yr]
