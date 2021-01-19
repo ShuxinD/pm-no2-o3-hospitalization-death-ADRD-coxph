5c295e409958da27fe4b977e545599ce60094e25
Shuxin Dong <shuxind11@gmail.com>
2021-01-18 22:17:11 -0700

    Update getEnrolledInfo.R

--- code/00generate_data/getEnrolledInfo.R
+++ code/00generate_data/getEnrolledInfo.R
@@ -2,7 +2,7 @@
 # Project: Air Pollution on mortality and readmission in Medicare AD/ADRD     #
 # Code: get ADRD study population, exclude problematic ID                     #
 # Input: "hospital_total.rds", "no_crosswalk_no_death_ids.fst"                #
-# Output: "enrollINFO.csv" - IDs and first year of ADRD admission             #
+# Output: "enrolledINFO.csv" - IDs and first year of ADRD admission           #
 # Author: Shuxin Dong                                                         #
 # Date: 2021-01-18                                                            #
 ###############################################################################
@@ -29,12 +29,12 @@ ADRDmed <- subset(med, Alzheimer_pdx==1|Alzheimer_pdx2dx_10==1|Alzheimer_pdx2dx_
                   select = c("QID", "ADATE"))
 ADRDmed[, year_admit := as.numeric(format(ADATE, "%Y"))][]
 
-enrollINFO <- ADRDmed[, list(firstADRDyr = min(year_admit)), by = .(QID)]
-setDT(enrollINFO)
+enrolledInfo <- ADRDmed[, list(firstADRDyr = min(year_admit)), by = .(QID)]
+setDT(enrolledInfo)
 ######################### 2. exclude problematic IDs ##########################
 probIDs <- read_fst(paste0(dir_input_crosswalk, "no_crosswalk_no_death_ids.fst"), 
                     as.data.table = T)
 probIDs[, old_id]
-enrollINFO <- enrollINFO[!(QID %in% probIDs[,old_id]),] # exclude problematic IDs
+enrolledInfo <- enrolledInfo[!(QID %in% probIDs[,old_id]),] # exclude problematic IDs
 ######################### 3. save enrolled INFO ###############################
-fwrite(enrollINFO, paste0(dir_output, "enrollINFO.csv"))
+fwrite(enrolledInfo, paste0(dir_output, "enrolledInfo.csv"))
