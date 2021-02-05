13b703b52b2815632bc0461b68986786e6a84cda
Shuxin Dong <shuxind11@gmail.com>
2021-02-05 11:57:45 -0500

    Create 04mergeExposure.R

---
+++ code/00generate_data/04mergeExposure.R
@@ -0,0 +1,46 @@
+###############################################################################
+# Project: Air Pollution on mortality and readmission in Medicare AD/ADRD     #
+# Code: extract ADRD population based on enrolled info from denominator files #
+# Input: "ADRDpeople_denom.csv"
+# Input: NO2 and ozone data                             
+# Output: "ADRDpeople.csv" 
+# Author: Shuxin Dong                                                         #
+# Date: 2021-02-05                                                            #
+###############################################################################
+
+############################# 0. Setup ########################################
+rm(list = ls())
+gc()
+
+library(NSAPHutils)
+set_threads()
+library(data.table)
+setDTthreads(threads = 0)
+library(fst)
+
+dir_no2 <- "/nfs/nsaph_ci3/ci3_exposure/no2/whole_us/annual/zipcode/qd_predictions_ensemble/ywei_aggregations/"
+dir_ozone <- "/nfs/nsaph_ci3/ci3_exposure/ozone/whole_us/annual/zipcode/requaia_predictions/ywei_aggregation/"
+dir_denom <- "/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/"
+
+dir_out <- "/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/"
+
+############################# 1. merge ########################################
+ADRDpeople <- fread(paste0(dir_denom, "ADRDpeople_denom.csv"))
+
+no2_data <- fread(paste0(dir_no2, "all_years.csv"))
+ozone_data <- fread(paste0(dir_ozone, "all_years.csv"))
+
+exposure <- merge(no2_data, ozone_data, by = c("ZIP", "year"))
+rm(no2_data)
+rm(ozone_data)
+gc()
+exposure[, ZIP]
+
+names(ADRDpeople)
+ADRDpeople[,zip]
+
+# ADRDpeople[exposure, on = .(zip = ZIP, year = year)]
+
+for (year_ in 2000:2016) {
+  write_fst(out[year == year_], paste0("../data/cache_dir/exposure_data/merged_exposure_", year_, ".fst"))
+}
