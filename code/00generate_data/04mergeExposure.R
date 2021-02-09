25b1c181e8f3c778a7c03a9a6fd3db229b693af1
Shuxin Dong <shuxind11@gmail.com>
2021-02-08 21:18:50 -0500

    Update 04mergeExposure.R

--- code/00generate_data/04mergeExposure.R
+++ code/00generate_data/04mergeExposure.R
@@ -2,7 +2,7 @@
 # Project: Air Pollution on mortality and readmission in Medicare AD/ADRD     #
 # Code: extract ADRD population based on enrolled info from denominator files #
 # Input: "ADRDpeople_denom.csv"
-# Input: NO2 and ozone data                             
+# Input: pm25 NO2 and ozone data                             
 # Output: "ADRDpeople.csv" 
 # Author: Shuxin Dong                                                         #
 # Date: 2021-02-05                                                            #
@@ -18,6 +18,7 @@ library(data.table)
 setDTthreads(threads = 0)
 library(fst)
 
+dir_pm25 <- "/nfs/nsaph_ci3/ci3_exposure/pm25/whole_us/annual/zipcode/qd_predictions_ensemble/ywei_aggregation/"
 dir_no2 <- "/nfs/nsaph_ci3/ci3_exposure/no2/whole_us/annual/zipcode/qd_predictions_ensemble/ywei_aggregations/"
 dir_ozone <- "/nfs/nsaph_ci3/ci3_exposure/ozone/whole_us/annual/zipcode/requaia_predictions/ywei_aggregation/"
 dir_denom <- "/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/"
@@ -26,20 +27,33 @@ dir_out <- "/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/"
 
 ############################# 1. merge ########################################
 ADRDpeople <- fread(paste0(dir_denom, "ADRDpeople_denom.csv"))
+names(ADRDpeople)
 
+pm25_data <- fread(paste0(dir_pm25, "all_years.csv"))
 no2_data <- fread(paste0(dir_no2, "all_years.csv"))
 ozone_data <- fread(paste0(dir_ozone, "all_years.csv"))
 
-exposure <- merge(no2_data, ozone_data, by = c("ZIP", "year"))
+exposure <- merge(pm25_data, no2_data, by = c("ZIP", "year"))
+exposure <- merge(exposure, ozone_data, by = c("ZIP", "year"))
+head(exposure)
+
+rm(pm25_data)
 rm(no2_data)
 rm(ozone_data)
 gc()
-exposure[, ZIP]
+head(exposure[, ZIP])
 
 names(ADRDpeople)
-ADRDpeople[,zip]
+# [1] "qid"                "year"               "zip"                "dead"               "sex"               
+# [6] "race"               "age"                "dual"               "statecode"          "entry_age_break"   
+# [11] "mean_bmi"           "smoke_rate"         "hispanic"           "pct_blk"            "medhouseholdincome"
+# [16] "medianhousevalue"   "poverty"            "education"          "popdensity"         "pct_owner_occ"     
+# [21] "firstADRDyr"  
+
+head(ADRDpeople[,zip])
 
 # ADRDpeople[exposure, on = .(zip = ZIP, year = year)]
 combined <- merge(ADRDpeople, exposure, by.x = c("zip", "year"), by.y = c("ZIP", "year"), all.x = TRUE)
+head(combined)
 
 fwrite(combined, paste0(dir_out, "ADRDpeople.csv"))
