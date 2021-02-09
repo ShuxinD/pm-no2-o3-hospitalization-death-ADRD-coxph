5a6617433c1f7e30545bb83304d6ddcfe769787f
Shuxin Dong <shuxind11@gmail.com>
2021-02-09 10:48:42 -0500

    Update 01descriptive.R

--- code/01descriptive.R
+++ code/01descriptive.R
@@ -1,11 +1,11 @@
 ###############################################################################
-# Project: Medicare Mortality and Air Pollution in AD/ADRD                    #
+# Project: Air Pollution and mortality / readmission in AD/ADRD Medicare      #
 # Code: covariates correlations, create table one                             #
-# Input: "ADRDmort_cplt.csv"                                                  #
+# Input: "ADRD_mortality.csv"                                                  
 # Output: "corr.csv" as correlations between covariates                       #
 # Output: "table1.doc"                                                        #
 # Author: Shuxin Dong                                                         #
-# Date: Dec 9, 2020                                                           #
+# Date: 2021-02-08                                                            #
 ###############################################################################
 
 ############################# 0. Setup ########################################
@@ -13,60 +13,54 @@ rm(list = ls())
 gc()
 
 library(data.table)
-library(dplyr)
+setDTthreads(threads = 0)
 
 setwd("/nfs/home/S/shd968/shared_space/ci3_shd968/dementia")
 
-dir_data <- "/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/"
-dir_output <- "/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/descrip/"
+dir_in <- "/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/"
+dir_out <- "/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/"
 
-setDTthreads(threads = 0)
-
-dt <- fread(paste0(dir_data, "ADRDmort_cplt.csv"))
+dt <- fread(paste0(dir_in, "ADRD_mortality.csv"))
 names(dt)
 # > names(dt)
-# [1] "QID"                 "year"                "zip"                 "AGE"                 "STATECODE"          
-# [6] "Sex_gp"              "age_gp"              "Dual_gp"             "race"                "bene_dod"           
-# [11] "ADATE"               "DDATE"               "DIAG1"               "DIAG2"               "diabetes"           
-# [16] "year_admit"          "pm25"                "no2"                 "PctEye"              "PctLDL"             
-# [21] "Pctmam"              "LungCancerRate"      "poverty"             "popdensity"          "medianhousevalue"   
-# [26] "pct_blk"             "medhouseholdincome"  "pct_owner_occ"       "hispanic"            "education"          
-# [31] "smoke_rate"          "mean_bmi"            "amb_visit_pct"       "a1c_exm_pct"         "nearest_hospital_km"
-# [36] "ozone"               "firstADRDyr"         "mort_yr"             "death" 
+# [1] "zip"                "year"               "qid"                "dead"               "sex"               
+# [6] "race"               "age"                "dual"               "statecode"          "entry_age_break"   
+# [11] "mean_bmi"           "smoke_rate"         "hispanic"           "pct_blk"            "medhouseholdincome"
+# [16] "medianhousevalue"   "poverty"            "education"          "popdensity"         "pct_owner_occ"     
+# [21] "firstADRDyr"        "pm25"               "no2"                "ozone"   
 
 ########################## 1. calculate corr ##################################
-corr_data <- dt[,.(PctEye, PctLDL, Pctmam, LungCancerRate, poverty, 
-                   popdensity, medianhousevalue, pct_blk, 
-                   medhouseholdincome, pct_owner_occ, hispanic,
-                   education, smoke_rate, mean_bmi, amb_visit_pct,
-                   a1c_exm_pct, nearest_hospital_km, pm25, no2, ozone)]
+corr_data <- dt[,.(race, dual, mean_bmi, smoke_rate, hispanic,
+                   pct_blk, medhouseholdincome, medianhousevalue, poverty,
+                   education, popdensity, pct_owner_occ, pm25, no2, ozone)]
 corr <- cor(corr_data)
-write.csv(corr, file = paste0(dir_output, "corr.csv"))
+write.csv(corr, file = paste0(dir_out, "corr.csv"))
 
 ############################# 2. create table one #############################
-listVars <- c("pm25", "no2", "ozone", "po", "poverty", "popdensity",
-              "medhouseholdincome", "education", "pct_blk", "PctEye",
-              "PctLDL", "Pctmam", "LungCancerRate", "medianhousevalue", 
-              "pct_owner_occ", "hispanic", "smoke_rate", "mean_bmi",
-              "amb_visit_pct","a1c_exm_pct", "nearest_hospital_km")
+listVars <- c("pm25", "no2", "ozone", 
+              "mean_bmi", "smoke_rate", "hispanic", "pct_blk",
+              "medhouseholdincome", "medianhousevalue", "poverty",
+              "education", "popdensity", "pct_owner_occ")
 table1.personyr <- tableone::CreateTableOne(vars = listVars, 
                                             data = dt)
 table1.personyr <- print(table1.personyr)
 
 ## for individual
-dt_ind <- dt[,.SD[1], by=QID]
+head(dt)
+setorder(dt, qid, year)
+dt_ind <- dt[,.SD[1], by=qid]
 head(dt_ind)
-dt_ind <- dt_ind[,.(QID, AGE, age_gp, year_admit, Sex_gp, Dual_gp, race, death)]
+dt_ind <- dt_ind[,.(qid, sex, race, age, entry_age_break, dual, year)]
 
-listVars <- c("death", "Sex_gp", "race", "Dual_gp", "AGE", "age_gp" ,"year_admit")
-catVars <- c("death", "Sex_gp", "age_gp", "race", "Dual_gp", "year_admit")
+listVars <- c("sex", "race", "age", "entry_age_break", "dual", "year")
+catVars <- c("sex", "race", "entry_age_break", "dual", "year")
 table1.individual <- tableone::CreateTableOne(vars = listVars, 
                                               data = dt_ind, 
                                               factorVars = catVars)
 table1.individual <- print(table1.individual)
 
 library(rtf)
-rtffile <- RTF(paste0(dir_output, "table1.doc"))
+rtffile <- RTF(paste0(dir_out, "table1.doc"))
 addParagraph(rtffile, "Table1_person year")
 addTable(rtffile, cbind(rownames(table1.personyr), table1.personyr))
 addParagraph(rtffile, "\n \n Table1_individual")
