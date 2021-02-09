53df4ad289e14982d271478b014fb2e5940fc952
Shuxin Dong <shuxind11@gmail.com>
2021-02-09 13:00:28 -0500

    Update 06cleanData.R

--- code/00generate_data/06cleanData.R
+++ code/00generate_data/06cleanData.R
@@ -26,30 +26,37 @@ names(ADRDpeople)
 # [16] "medianhousevalue"   "poverty"            "education"          "popdensity"         "pct_owner_occ"     
 # [21] "firstADRDyr"        "pm25"               "no2"                "ozone"    
 dt <- na.omit(ADRDpeople)[year!=firstADRDyr, ] # remove NAs and firstADRDyr
+uniqueN(dt, by = "qid")
+# [1] 5162518
 
 ########################### 2. Clean data #####################################
 temp <- dt[, .(start_yr = min(year),
                end_yr = max(year),
-               count = uniqueN(year)), by = qid]
-temp <- merge(temp, dt[,.(qid,firstADRDyr)], by = "qid", all.x = TRUE)
+               count = uniqueN(year)), by = .(qid)]
+dim(temp)
+head(temp)
+temp <- merge(temp, unique(dt[,.(qid,firstADRDyr)]), by = "qid", all.x = TRUE)
+dim(temp)
 gc()
 
 ## remove those not followed-up from the year following firstADRDyr
-temp[start_yr != (firstADRDyr+1)]
+dim(temp[start_yr != (firstADRDyr+1)]) # number of subjects to remove
+# [1] 65290     5
 dt[!(qid %in% temp[start_yr != (firstADRDyr+1)][, qid]), ]
-dt <- dt[!(qid %in% temp[start_yr != (firstADRDyr+1)][, qid]), ]
+dt <- dt[qid %in% temp[start_yr == (firstADRDyr+1)][, qid], ]
 gc()
 
-dim(temp)[1] - dim(dt)[1]
+dim(ADRDpeople)[1] - dim(dt)[1] # number of person-years to removed
 # [1] 218613
 
 ## remove those not having each year’s info during follow-up
-dt[!(qid %in% temp[(end_yr-start_yr+1) != count,][,qid]),]
-dt <- dt[!(qid %in% temp[(end_yr-start_yr+1) != count,][,qid]),]
+dim(temp[(end_yr-start_yr+1) != count,]) # number of subjects to remove
+# [1] 14804     5
+dt <- dt[qid %in% temp[(end_yr-start_yr+1) == count, qid],]
 gc()
 
-dim(temp)[1] - dim(dt)[1]
-# [1] 297473
+dim(ADRDpeople)[1] - dim(dt)[1]
+# [1] 10235313
 
 head(dt)
 
