3340f9ee6ae87112950e3abfdb99c227402aaaa2
Shuxin Dong <shuxind11@gmail.com>
2021-03-03 13:19:35 -0500

    Update 06cleanData.R

--- code/00generate_data/06cleanData.R
+++ code/00generate_data/06cleanData.R
@@ -20,14 +20,19 @@ dir_out <- "/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/"
 ########################### 1. Load data ######################################
 ADRDpeople <- fread(paste0(dir_in, "ADRDpeople.csv"))
 names(ADRDpeople)
-# [1] "zip"                "year"               "qid"                "dead"               "sex"               
-# [6] "race"               "age"                "dual"               "statecode"          "entry_age_break"   
-# [11] "mean_bmi"           "smoke_rate"         "hispanic"           "pct_blk"            "medhouseholdincome"
-# [16] "medianhousevalue"   "poverty"            "education"          "popdensity"         "pct_owner_occ"     
-# [21] "firstADRDyr"        "pm25"               "no2"                "ozone"    
+# [1] "zip"                "year"               "qid"                "summer_tmmx"        "winter_tmmx"        "summer_rmax"       
+# [7] "winter_rmax"        "dead"               "sex"                "race"               "age"                "dual"              
+# [13] "statecode"          "entry_age_break"    "mean_bmi"           "smoke_rate"         "hispanic"           "pct_blk"           
+# [19] "medhouseholdincome" "medianhousevalue"   "poverty"            "education"          "popdensity"         "pct_owner_occ"     
+# [25] "firstADRDyr"        "pm25"               "no2"                "ozone"  
 dt <- na.omit(ADRDpeople)[year!=firstADRDyr, ] # remove NAs and firstADRDyr
+dim(dt)
+# [1] 17081631       28
+dim(ADRDpeople)[1] - dim(dt)[1] # number of person-years to be removed due to NAs
+# > dim(ADRDpeople)[1] - dim(dt)[1]
+# [1] 9967865
 uniqueN(dt, by = "qid")
-# [1] 5162518
+# [1] 5154447
 
 ########################### 2. Clean data #####################################
 temp <- dt[, .(start_yr = min(year),
@@ -37,26 +42,27 @@ dim(temp)
 head(temp)
 temp <- merge(temp, unique(dt[,.(qid,firstADRDyr)]), by = "qid", all.x = TRUE)
 dim(temp)
+
 gc()
 
 ## remove those not followed-up from the year following firstADRDyr
-dim(temp[start_yr != (firstADRDyr+1)]) # number of subjects to remove
-# [1] 65290     5
+dim(temp[start_yr > (firstADRDyr+1)]) # number of subjects to remove
+# [1] 64530     5
 dt[!(qid %in% temp[start_yr != (firstADRDyr+1)][, qid]), ]
-dt <- dt[qid %in% temp[start_yr == (firstADRDyr+1)][, qid], ]
+dt <- dt[!(qid %in% temp[start_yr != (firstADRDyr+1)][, qid]), ]
 gc()
 
 dim(ADRDpeople)[1] - dim(dt)[1] # number of person-years to removed
-# [1] 218613
+# [1] 10183511
 
 ## remove those not having each year’s info during follow-up
 dim(temp[(end_yr-start_yr+1) != count,]) # number of subjects to remove
-# [1] 14804     5
+# [1] 13669     5
 dt <- dt[qid %in% temp[(end_yr-start_yr+1) == count, qid],]
 gc()
 
 dim(ADRDpeople)[1] - dim(dt)[1]
-# [1] 10235313
+# [1] 10258411
 
 head(dt)
 
