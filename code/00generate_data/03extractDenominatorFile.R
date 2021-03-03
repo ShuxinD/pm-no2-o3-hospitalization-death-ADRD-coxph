c199e194c969f1a04582acb644758ae63e6061e6
Shuxin Dong <shuxind11@gmail.com>
2021-03-03 13:19:29 -0500

    Update 03extractDenominatorFile.R

--- code/00generate_data/03extractDenominatorFile.R
+++ code/00generate_data/03extractDenominatorFile.R
@@ -27,10 +27,27 @@ f <- list.files(dir_denominator, pattern = "\\.fst", full.names = TRUE)
 ## example to read fst file
 # example <- read_fst(f[1])
 # names(example)
-myvars <- c("year", "zip", "sex", "race", "age", "dual", "entry_age_break", "statecode",
-            "dead",
-            "mean_bmi", "smoke_rate", "hispanic", "pct_blk", "medhouseholdincome", "medianhousevalue",
-            "poverty", "education", "popdensity", "pct_owner_occ",
+# > names(example)
+# [1] "zip"                          "year"                         "qid"                          "dodflag"                     
+# [5] "bene_dod"                     "sex"                          "race"                         "age"                         
+# [9] "hmo_mo"                       "hmoind"                       "statecode"                    "latitude"                    
+# [13] "longitude"                    "dual"                         "death"                        "dead"                        
+# [17] "entry_age"                    "entry_year"                   "entry_age_break"              "followup_year"               
+# [21] "followup_year_plus_one"       "pm25_ensemble"                "pm25_no_interp"               "pm25_nn"                     
+# [25] "ozone"                        "ozone_no_interp"              "zcta"                         "poverty"                     
+# [29] "popdensity"                   "medianhousevalue"             "pct_blk"                      "medhouseholdincome"          
+# [33] "pct_owner_occ"                "hispanic"                     "education"                    "population"                  
+# [37] "zcta_no_interp"               "poverty_no_interp"            "popdensity_no_interp"         "medianhousevalue_no_interp"  
+# [41] "pct_blk_no_interp"            "medhouseholdincome_no_interp" "pct_owner_occ_no_interp"      "hispanic_no_interp"          
+# [45] "education_no_interp"          "population_no_interp"         "smoke_rate"                   "mean_bmi"                    
+# [49] "smoke_rate_no_interp"         "mean_bmi_no_interp"           "amb_visit_pct"                "a1c_exm_pct"                 
+# [53] "amb_visit_pct_no_interp"      "a1c_exm_pct_no_interp"        "tmmx"                         "rmax"                        
+# [57] "pr"                           "cluster_cat"                  "fips_no_interp"               "fips"                        
+# [61] "summer_tmmx"                  "summer_rmax"                  "winter_tmmx"                  "winter_rmax" 
+myvars <- c("qid", "year", #"zip", "sex", "race", "age", "dual", "entry_age_break", "statecode",
+            #"dead",
+            #"mean_bmi", "smoke_rate", "hispanic", "pct_blk", "medhouseholdincome", "medianhousevalue",
+            #"poverty", "education", "popdensity", "pct_owner_occ",
             "summer_tmmx", "winter_tmmx", "summer_rmax", "winter_rmax")
 dt <- rbindlist(lapply(f,
                        read_fst,
@@ -49,7 +66,7 @@ uniqueN(ADRDpeople[,qid])
 dim(ADRDpeople)
 # [1] 87476969       20
 
-ADRDpeople <- merge(ADRDpeople, enrolledInfo, by.x = "qid", by.y = "QID")
+ADRDpeople <- merge(ADRDpeople, enrolledInfo, by.x = "qid", by.y = "QID", all = FALSE)
 ADRDpeople <- ADRDpeople[year >= firstADRDyr, ]
 uniqueN(ADRDpeople[,qid])
 # [1] 8037303
