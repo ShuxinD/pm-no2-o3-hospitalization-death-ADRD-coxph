d040e46cbdbdb07a4ee6250f2ce8e53140ef48be
Shuxin Dong <shuxind11@gmail.com>
2021-03-30 16:19:36 -0400

    Update 03poisson.R

--- codes/03poisson.R
+++ codes/03poisson.R
@@ -6,6 +6,7 @@
 # Author: Shuxin Dong                                                         #
 # Date: 2021-02-17                                                            #
 ###############################################################################
+## set core as 24
 
 ############################# 0. Setup ########################################
 rm(list = ls())
@@ -14,6 +15,10 @@ gc()
 library(data.table)
 setDTthreads(threads = 0)
 
+library(parallel)
+library(mgcv)
+# numCores <- detectCores()
+
 setwd("/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/")
 
 dir_data <- "/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/"
@@ -21,16 +26,9 @@ dir_results <- "/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/"
 
 dt <- fread(paste0(dir_data, "ADRD_mortality.csv"))
 names(dt)
-# > names(dt)
-# [1] "zip"                "year"               "qid"                "summer_tmmx"        "winter_tmmx"        "summer_rmax"       
-# [7] "winter_rmax"        "dead"               "sex"                "race"               "age"                "dual"              
-# [13] "statecode"          "entry_age_break"    "mean_bmi"           "smoke_rate"         "hispanic"           "pct_blk"           
-# [19] "medhouseholdincome" "medianhousevalue"   "poverty"            "education"          "popdensity"         "pct_owner_occ"     
-# [25] "firstADRDyr"        "pm25"               "no2"                "ozone"  
 
-dt[, followupyr := (year - firstADRDyr)][]
-dt[, strata := I(as.factor(entry_age_break):as.factor(sex):as.factor(race):as.factor(dual))][]
-head(dt)
+dt[, followupyr := (year - firstADRDyr)]
+dt[, statecode := as.factor(statecode)]
 
 NORTHEAST <- c("NY", "MA", "PA", "RI", "NH", "ME", "VT", "CT", "NJ")  
 SOUTH <- c("DC", "VA", "NC", "WV", "KY", "SC", "GA", "FL", "AL", "TN", "MS", 
@@ -43,74 +41,212 @@ dt$region <- ifelse(dt$statecode %in% NORTHEAST, "NORTHEAST",
                            ifelse(dt$statecode  %in% MIDWEST, "MIDWEST",
                                   ifelse(dt$statecode  %in% WEST, "WEST",
                                          NA))))
+dt[, region:=as.factor(region)]
+head(dt)
 gc()
 
-IQRs <- data.table(IQR(dt$pm25), IQR(dt$no2), IQR(dt$ozone))
-colnames(IQRs) <- c("pm25", "no2", "ozone")
+IQRs <- data.table(IQR(dt$pm25), IQR(dt$no2), IQR(dt$ozone), IQR(dt$ox))
+colnames(IQRs) <- c("pm25", "no2", "ozone", "ox")
 print(IQRs)
 
-############################# 1. fit models ###################################
-library(parallel)
-library(mgcv)
-numCores <- detectCores()
-cl <- makeCluster(numCores-1)
+#################### 1. single-pollutant models ###############################
+cl <- makeCluster(23)
 
-# cox_raw <- coxph(Surv(time = followupyr, time2 = followupyr_plusone, event = dead) ~
-#                    pm25 + no2 + ozone +
-#                    mean_bmi + smoke_rate + hispanic + pct_blk +
-#                    medhouseholdincome + medianhousevalue +
-#                    poverty + education + popdensity + pct_owner_occ +
-#                    summer_tmmx + winter_tmmx + summer_rmax + winter_rmax +
-#                    as.factor(year) +  as.factor(region) +
-#                    strata(as.factor(entry_age_break)) + strata(as.factor(sex)) +
-#                    strata(as.factor(race)) + strata(as.factor(dual)),
-#                  data = dt,
-#                  tie = c("efron"),
-#                  na.action = na.omit)
+p_1_pm25 <- bam(dead ~ s(followupyr, by = as.factor(entry_age_break)) + 
+                  s(followupyr, by = as.factor(sex)) +
+                  s(followupyr, by = as.factor(race)) + 
+                  s(followupyr, by = as.factor(dual)) +
+                  pm25 + 
+                  mean_bmi + smoke_rate + hispanic + pct_blk +
+                  medhouseholdincome + medianhousevalue +
+                  poverty + education + popdensity + pct_owner_occ +
+                  summer_tmmx + winter_tmmx + summer_rmax + winter_rmax +
+                  as.factor(year) + as.factor(region) +
+                  s(region, bs = "re"),
+                family = poisson(), data = dt,
+                discrete = TRUE,
+                nthreads = length(cl))
+tb <- summary(p_1_pm25)$p.table
+tb <- as.data.frame(tb)
+setDT(tb, keep.rownames = TRUE)[]
+fwrite(tb, paste0(dir_results, "poisson_1_pm25.csv"))
 
-p_raw <- bam(dead ~ s(followupyr, by = "strata") + 
-                 pm25 + no2 + ozone + 
+IQRunit <- c(IQRs$pm25)
+HR <- tb[2,]
+HR <- cbind(HR,IQRunit)
+print(HR)
+HR[, `:=`(HR_IQR = exp(Estimate*IQRunit),
+          HR_lci = exp((Estimate-1.96*`Std. Error`)*IQRunit),
+          HR_uci = exp((Estimate+1.96*`Std. Error`)*IQRunit))][]
+fwrite(HR, paste0(dir_results, "poisson_1_pm25_HR.csv"))
+gc()
+
+p_1_no2 <- bam(dead ~ s(followupyr, by = as.factor(entry_age_break)) + 
+                 s(followupyr, by = as.factor(sex)) +
+                 s(followupyr, by = as.factor(race)) + 
+                 s(followupyr, by = as.factor(dual)) +
+                 no2 + 
                  mean_bmi + smoke_rate + hispanic + pct_blk +
                  medhouseholdincome + medianhousevalue +
                  poverty + education + popdensity + pct_owner_occ +
                  summer_tmmx + winter_tmmx + summer_rmax + winter_rmax +
-                 as.factor(year) + as.factor(region),
-               family="poisson", data = dt,
-               cluster = cl, nthreads = NA)
-# saveRDS(p_raw, paste0(dir_results, "p_raw.rds"))
-tb <- summary(cox_raw)$coefficients
+                 as.factor(year) + as.factor(region) +
+                 s(region, bs = "re"),
+               family = poisson(), data = dt,
+               discrete = TRUE,
+               nthreads = length(cl))
+tb <- summary(p_1_no2)$p.table
 tb <- as.data.frame(tb)
 setDT(tb, keep.rownames = TRUE)[]
-fwrite(tb, paste0(dir_results, "poisson_raw.csv"))
+fwrite(tb, paste0(dir_results, "poisson_1_no2.csv"))
 
-IQRunit <- c(IQRs$pm25, IQRs$no2, IQRs$ozone)
-HR <- tb[1:3,]
+IQRunit <- c(IQRs$no2)
+HR <- tb[2,]
 HR <- cbind(HR,IQRunit)
 print(HR)
-HR[, `:=`(HR_IQR = exp(coef*IQRunit),
-          HR_lci = exp((coef-1.96*`se(coef)`)*IQRunit),
-          HR_uci = exp((coef+1.96*`se(coef)`)*IQRunit))][]
-fwrite(HR, paste0(dir_results, "poisson_raw_HR.csv"))
+HR[, `:=`(HR_IQR = exp(Estimate*IQRunit),
+          HR_lci = exp((Estimate-1.96*`Std. Error`)*IQRunit),
+          HR_uci = exp((Estimate+1.96*`Std. Error`)*IQRunit))][]
+fwrite(HR, paste0(dir_results, "poisson_1_no2_HR.csv"))
 
+
+p_1_ozone <- bam(dead ~ s(followupyr, by = as.factor(entry_age_break)) + 
+                   s(followupyr, by = as.factor(sex)) +
+                   s(followupyr, by = as.factor(race)) + 
+                   s(followupyr, by = as.factor(dual)) +
+                   ozone + 
+                   mean_bmi + smoke_rate + hispanic + pct_blk +
+                   medhouseholdincome + medianhousevalue +
+                   poverty + education + popdensity + pct_owner_occ +
+                   summer_tmmx + winter_tmmx + summer_rmax + winter_rmax +
+                   as.factor(year) + as.factor(region) +
+                   s(region, bs = "re"),
+                 family = poisson(), data = dt,
+                 discrete = TRUE,
+                 nthreads = length(cl))
+tb <- summary(p_1_ozone)$p.table
+tb <- as.data.frame(tb)
+setDT(tb, keep.rownames = TRUE)[]
+fwrite(tb, paste0(dir_results, "poisson_1_ozone.csv"))
+
+IQRunit <- c(IQRs$ozone)
+HR <- tb[2,]
+HR <- cbind(HR,IQRunit)
+print(HR)
+HR[, `:=`(HR_IQR = exp(Estimate*IQRunit),
+          HR_lci = exp((Estimate-1.96*`Std. Error`)*IQRunit),
+          HR_uci = exp((Estimate+1.96*`Std. Error`)*IQRunit))][]
+fwrite(HR, paste0(dir_results, "poisson_1_ozone_HR.csv"))
+
+p_1_ox <- bam(dead ~ s(followupyr, by = as.factor(entry_age_break)) + 
+                   s(followupyr, by = as.factor(sex)) +
+                   s(followupyr, by = as.factor(race)) + 
+                   s(followupyr, by = as.factor(dual)) +
+                   ox + 
+                   mean_bmi + smoke_rate + hispanic + pct_blk +
+                   medhouseholdincome + medianhousevalue +
+                   poverty + education + popdensity + pct_owner_occ +
+                   summer_tmmx + winter_tmmx + summer_rmax + winter_rmax +
+                   as.factor(year) + as.factor(region) +
+                   s(region, bs = "re"),
+                 family = poisson(), data = dt,
+                 discrete = TRUE,
+                 nthreads = length(cl))
+tb <- summary(p_1_ox)$p.table
+tb <- as.data.frame(tb)
+setDT(tb, keep.rownames = TRUE)[]
+fwrite(tb, paste0(dir_results, "poisson_1_ox.csv"))
+
+IQRunit <- c(IQRs$ox)
+HR <- tb[2,]
+HR <- cbind(HR,IQRunit)
+print(HR)
+HR[, `:=`(HR_IQR = exp(Estimate*IQRunit),
+          HR_lci = exp((Estimate-1.96*`Std. Error`)*IQRunit),
+          HR_uci = exp((Estimate+1.96*`Std. Error`)*IQRunit))][]
+fwrite(HR, paste0(dir_results, "poisson_1_ox_HR.csv"))
+
+#################### 2. multi-pollutants models ###############################
+p_1_all3 <- bam(dead ~ s(followupyr, by = as.factor(entry_age_break)) + 
+             s(followupyr, by = as.factor(sex)) +
+             s(followupyr, by = as.factor(race)) + 
+             s(followupyr, by = as.factor(dual)) +
+             pm25 + no2 + ozone + 
+             mean_bmi + smoke_rate + hispanic + pct_blk +
+             medhouseholdincome + medianhousevalue +
+             poverty + education + popdensity + pct_owner_occ +
+             summer_tmmx + winter_tmmx + summer_rmax + winter_rmax +
+             as.factor(year) + as.factor(region) +
+             s(region, bs = "re"),
+           family = poisson(), data = dt,
+           discrete = TRUE,
+           nthreads = length(cl))
+tb <- summary(p_1_all3)$p.table
+tb <- as.data.frame(tb)
+setDT(tb, keep.rownames = TRUE)[]
+fwrite(tb, paste0(dir_results, "poisson_1_all3_coef.csv"))
+
+IQRunit <- c(IQRs$pm25, IQRs$no2, IQRs$ozone)
+HR <- tb[2:4,]
+HR <- cbind(HR,IQRunit)
+print(HR)
+HR[, `:=`(HR_IQR = exp(Estimate*IQRunit),
+          HR_lci = exp((Estimate-1.96*`Std. Error`)*IQRunit),
+          HR_uci = exp((Estimate+1.96*`Std. Error`)*IQRunit))][]
+fwrite(HR, paste0(dir_results, "poisson_1_all3_HR.csv"))
 gc()
 
-p_splines <- bam(dead ~ s(followupyr, by = "strata") + 
-               s(pm25) + s(no2) + s(ozone) + 
-               mean_bmi + smoke_rate + hispanic + pct_blk +
-               medhouseholdincome + medianhousevalue +
-               poverty + education + popdensity + pct_owner_occ +
-               summer_tmmx + winter_tmmx + summer_rmax + winter_rmax +
-               as.factor(year) + as.factor(region),
-             family="poisson", data = dt,
-             cluster = cl, nthreads = NA)
+p_1_all2 <- bam(dead ~ s(followupyr, by = as.factor(entry_age_break)) + 
+                  s(followupyr, by = as.factor(sex)) +
+                  s(followupyr, by = as.factor(race)) + 
+                  s(followupyr, by = as.factor(dual)) +
+                  pm25 + ox +
+                  mean_bmi + smoke_rate + hispanic + pct_blk +
+                  medhouseholdincome + medianhousevalue +
+                  poverty + education + popdensity + pct_owner_occ +
+                  summer_tmmx + winter_tmmx + summer_rmax + winter_rmax +
+                  as.factor(year) + as.factor(region) +
+                  s(region, bs = "re"),
+                family = poisson(), data = dt,
+                discrete = TRUE,
+                nthreads = length(cl))
+tb <- summary(p_1_all2)$p.table
+tb <- as.data.frame(tb)
+setDT(tb, keep.rownames = TRUE)[]
+fwrite(tb, paste0(dir_results, "poisson_1_all2_coef.csv"))
 
-# s.P_all <- bam(dead ~ s(followupyr, by = "strata") + 
-#                  pm25 + no2 + ozone + 
-#                  s(age) +
-#                  s(mean_bmi) + s(smoke_rate) + s(hispanic) + s(pct_blk) +
-#                  s(medhouseholdincome) + s(medianhousevalue) +
-#                  s(poverty) + s(education) + s(popdensity) + s(pct_owner_occ) +
-#                  s(summer_tmmx) + s(winter_tmmx) + s(summer_rmax) + s(winter_rmax) +
-#                  as.factor(year),
-#                family="poisson", data = dt,
-#                cluster = cl, nthreads = NA)
+IQRunit <- c(IQRs$pm25, IQRs$ozone)
+HR <- tb[2:3,]
+HR <- cbind(HR,IQRunit)
+print(HR)
+HR[, `:=`(HR_IQR = exp(Estimate*IQRunit),
+          HR_lci = exp((Estimate-1.96*`Std. Error`)*IQRunit),
+          HR_uci = exp((Estimate+1.96*`Std. Error`)*IQRunit))][]
+fwrite(HR, paste0(dir_results, "poisson_1_all3_HR.csv"))
+gc()
+
+stop(cl)
+
+
+
+# vcov_mod <- vcovHC(p_1)
+# fwrite
+
+
+
+
+# p_splines <- bam(dead ~ s(followupyr, by = as.factor(entry_age_break)) + 
+#                    s(followupyr, by = as.factor(sex)) +
+#                    s(followupyr, by = as.factor(race)) + 
+#                    s(followupyr, by = as.factor(dual)) +
+#                    s(pm25, bs="ps") + s(no2, bs="ps") + s(ozone, bs="ps") + 
+#                    mean_bmi + smoke_rate + hispanic + pct_blk +
+#                    medhouseholdincome + medianhousevalue +
+#                    poverty + education + popdensity + pct_owner_occ +
+#                    summer_tmmx + winter_tmmx + summer_rmax + winter_rmax +
+#                    as.factor(year) + as.factor(statecode) +
+#                    s(as.factor(statecode), bs = "re"),
+#                  family = poisson(), data = dt,
+#                  discrete = TRUE,
+#                  nthreads = length(cl))
+
