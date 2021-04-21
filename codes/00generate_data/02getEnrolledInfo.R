d53817cc4d4d8832e0a570cf1f7f48044085e6ab
Shuxin Dong <43856307+ShuxinD@users.noreply.github.com>
2021-04-20 21:43:29 -0400

    Update 02getEnrolledInfo.R

--- codes/00generate_data/02getEnrolledInfo.R
+++ codes/00generate_data/02getEnrolledInfo.R
@@ -19,9 +19,9 @@ library(NSAPHutils)
 setDTthreads(threads = 0)
 setwd("/nfs/home/S/shd968/shared_space/ci3_shd968/dementia")
 
-dir_input_hospital <- "/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/ADRDhospitalization/"
+dir_input_hospital <- "/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/data/ADRDhospitalization/"
 dir_input_crosswalk <- "/nfs/home/S/shd968/shared_space/ci3_health_data/medicare/id_crosswalk/"
-dir_output <- "/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/"
+dir_output <- "/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/data/"
 
 ################ 1. combine hospitalization files together ####################
 ADRDhosp <- NULL
@@ -36,7 +36,12 @@ rm(adm_)
 gc()
 setDT(ADRDhosp)
 names(ADRDhosp)
-dim(ADRDhosp) # [1] 18823188       16
+# [1] "QID"            "ADATE"          "DDATE"          "DIAG1"          "DIAG2"         
+# [6] "DIAG3"          "DIAG4"          "DIAG5"          "DIAG6"          "DIAG7"         
+# [11] "DIAG8"          "DIAG9"          "DIAG10"         "year"           "ADRD_primary"  
+# [16] "ADRD_secondary"
+head(ADRDhosp)
+dim(ADRDhosp) # [1] 16482471       16
 
 any(duplicated(ADRDhosp))  # TRUE
 sum(duplicated(ADRDhosp)) # 1244244
@@ -69,3 +74,17 @@ dim(enrolledInfo)
 
 ######################### 3. save enrolled INFO ###############################
 fwrite(enrolledInfo, paste0(dir_output, "EnrolledInfo.csv"))
+
+setorder(ADRDhosp, QID, year)
+ADRDhosp[, ADATE:=NULL]
+ADRDhosp <- unique(ADRDhosp)
+ADRDhosp <- ADRDhosp[!(QID %in% probIDs[,old_id]),]
+
+setorder(ADRDhosp, QID, year)
+head(ADRDhosp, 10)
+ADRDhosp[,count:=.N, by = .(QID)]
+re_ADRDhosp <- ADRDhosp[count>1, ]
+re_ADRDhosp[, .SD[2], by = QID]
+re_ADRDhosp[, first_readmisson_yr := year][]
+re_ADRDhosp[, count := NULL][]
+fwrite(re_ADRDhosp, paste0(dir_output, "ReAdmissonInfo.csv"))
