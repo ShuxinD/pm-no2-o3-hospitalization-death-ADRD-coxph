10c83b15ebf77bd834cf6de81aed1c3db868b6d9
ShuxinD <shuxind11@gmail.com>
2021-11-03 15:14:33 -0400

    update descriptive stats for ReAd cohort

--- codes/00generate_data/07generateReAd.R
+++ codes/00generate_data/07generateReAd.R
@@ -40,13 +40,15 @@ for (year_ in 2000:2016) {
   
   write_fst(admissions, paste0(dir_cohortAd, "ADRDcohort_hospitalization", year_, ".fst"))
 }
+rm(admissions)
+gc()
 
 ## combine cohort hospitalization files together ----
 ADRDcohort_hosp <- NULL
 for (i in 2000:2016) {
-  adm_ <- read_fst( paste0(dir_cohortAd, "ADRDcohort_hospitalization", year_, ".fst"))
+  adm_ <- read_fst( paste0(dir_cohortAd, "ADRDcohort_hospitalization", i, ".fst"))
   ADRDcohort_hosp <- rbind(ADRDcohort_hosp, adm_)
-  cat("finish loading file:", "ADRDsecondary_", i,".fst", "\n")
+  cat("finish loading file:", "ADRDcohort_hospitalization", i,".fst", "\n")
 }
 rm(adm_)
 gc()
@@ -55,24 +57,24 @@ gc()
 setDT(ADRDcohort_hosp)
 names(ADRDcohort_hosp)
 #' year as Admission year
-dim(ADRDcohort_hosp) # [1] 16190443       31
+dim(ADRDcohort_hosp) # [1] 42421734       4
 
 any(duplicated(ADRDcohort_hosp))  # TRUE
-sum(duplicated(ADRDcohort_hosp)) # 1441 number of duplicates
+sum(duplicated(ADRDcohort_hosp)) # 4626 number of duplicates
 ADRDcohort_hosp <- unique(ADRDcohort_hosp) # remove all the duplicates
-dim(ADRDcohort_hosp) # [1] 16189002       31 # dimention after removing duplicates
+dim(ADRDcohort_hosp) # [1] 42417108       31 # dimention after removing duplicates
 gc()
 
-ADRDcohort_hosp_time <- ADRDhosp[,.(QID, year)] # drop all diagnosis codes, only save QID, admission year
+ADRDcohort_hosp_time <- ADRDcohort_hosp[,.(QID, year)] # drop all diagnosis codes, only save QID, admission year
 gc()
 head(ADRDcohort_hosp_time)
 ADRDcohort_hosp_time <- unique(ADRDcohort_hosp_time) # remove duplicate of Admission year (some may be admitted several times in one year)
-dim(ADRDcohort_hosp_time) # [1] 11843820        2
+dim(ADRDcohort_hosp_time) # [1] 23986044        2
 gc()
 
 ADRDcohort_hosp_time <- merge(ADRDcohort_hosp_time, enrollInfo, by = "QID", all.x=T) # merge firstADRDyr to cohort hospitalization
 ADRDcohort_hosp_time[year>firstADRDyr] #subset to those admitted after firstADRDyr
-ADRDcohort_hosp_time[year>firstADRDyr] <- ADRDcohort_hosp_time[year>firstADRDyr]
+ADRDcohort_hosp_time <- ADRDcohort_hosp_time[year>firstADRDyr]
 
 ## generate ReAd info ----
 setorder(ADRDcohort_hosp_time, QID, year)
@@ -80,21 +82,30 @@ ReAdInfo <- ADRDcohort_hosp_time[, .(firstReAdyr = min(year)), by = QID]
 dim(ReAdInfo)
 
 table(ReAdInfo[,firstReAdyr])
+# 2001   2002   2003   2004   2005   2006   2007   2008   2009   2010   2011   2012   2013   2014   2015 
+# 229983 263921 276067 275412 280113 269290 262623 255965 246445 247963 249083 253281 178504 147539 134839 
+# 2016 
+# 162505
 fwrite(ReAdInfo, paste0(dir_in, "ReAdmissionInfo.csv"))
 
 ## load ADRD cohort data ---
 cohort <- read_fst(paste0(dir_in, "ADRDcohort_clean.fst"), as.data.table = T)
 
 ## subset ----
+#' those with ReAd, followup until ReAd
 dt_ReAd_event <- cohort[qid %in% ReAdInfo[,QID],]
 # uniqueN(dt_ReAd_event[,qid])
 # uniqueN(ReAdInfo[,QID])
 dt_ReAd_event <- merge(dt_ReAd_event, ReAdInfo, by.x = "qid", by.y = "QID", all.x = TRUE)
 head(dt_ReAd_event)
+summary(dt_ReAd_event[,firstReAdyr]- dt_ReAd_event[,firstADRDyr])
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 1.00    1.00    2.00    2.24    3.00   16.00 
 dt_ReAd_event$ReAd <- FALSE
 dt_ReAd_event[year==firstReAdyr, ReAd:=TRUE][]
-dt_ReAd_event <- dt_ReAd_event[year<=first_ReAdyr, ]
+dt_ReAd_event <- dt_ReAd_event[year<=firstReAdyr, ]
 
+#' those without ReAd event, followup as the same as in mortality cohort
 dt_ReAd_noevent <- cohort[!(qid %in% ReAdInfo[,QID]),]
 head(dt_ReAd_noevent)
 dt_ReAd_noevent[, firstReAdyr := 0000][]
@@ -102,6 +113,6 @@ dt_ReAd_noevent[, ReAd := FALSE]
 
 dt_ReAd <- rbind(dt_ReAd_event, dt_ReAd_noevent)
 
-dt_ReAd[(dead)&(ReAd),]
-
+dim(dt_ReAd[(dead)&(ReAd),]) # the person-year death and readmission happened at the same time
+# [1] 1186923      36
 write_fst(dt_ReAd, paste0(dir_out, "ADRDcohort_ReAd.fst"))
