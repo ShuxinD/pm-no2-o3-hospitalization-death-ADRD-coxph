4fdbc3fc1f538d522253dbd516db2ae1aa235eb8
ShuxinD <shuxind11@gmail.com>
2022-07-20 17:47:10 -0400

    recreate the bootstrapping datasets

--- codes/bootstrapping/create_bootstrap_datasets.R
+++ codes/bootstrapping/create_bootstrap_datasets.R
@@ -1,6 +1,7 @@
 #' Project: airPollution_ADRD
 #' Code: bootstrapping dataset
-#' Input: "ADRDcohort_clean.fst"
+#' Input: "ADRDcohort_dead.fst"
+#' Input: "ADRDcohort_ReAd.fst"
 #' Output: bootstrap datasets
 #' Author: Shuxin Dong                                                         
 #' Date: 2021-07-07
@@ -12,25 +13,24 @@ gc()
 library(data.table)
 setDTthreads(threads = 0)
 library(fst)
-
-setwd("/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/")
-dir_data <- "/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/data/"
+#setwd("/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/")
+dir_data <- paste0(getwd(),"/data/")
 
 ## load mortality cohort ----
-dt <- read_fst(paste0(dir_data, "ADRDcohort_clean.fst"), as.data.table = T)
+dt <- read_fst(paste0(dir_data, "ADRDcohort_dead.fst"), as.data.table = T)
 all_zip <- unique(dt[,zip])
 num_uniq_zip <- uniqueN(dt[,zip])
 
 ## create bootstrap data for mortality ----
-#' Save the bootstrapped data to accelerate computing
-dir.create(file.path("/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/data/bootstrap/mortality/"), showWarnings = FALSE)
+## Save the bootstrapped data to accelerate computing
+dir.create(file.path(paste0(getwd(),"/data/bootstrap/mortality/")), showWarnings = FALSE)
 
-lapply(1:200, function(boots_id){
+lapply(1:100, function(boots_id){
   set.seed(boots_id)
   zip_sample <- sample(1:num_uniq_zip, floor((num_uniq_zip)^0.7), replace=T) 
   dt_boots <- subset(dt, zip %in% all_zip[zip_sample]) 
-  write_fst(dt_boots, paste0("/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/data/bootstrap/mortality/", boots_id,".fst"))
-  cat("finish creating data", boots_id, "of 200\n")
+  write_fst(dt_boots, paste0(getwd(),"/data/bootstrap/mortality/",boots_id,".fst"))
+  cat("finish creating data", boots_id, "of 100\n")
 })
 
 ## load ReAd cohort ----
@@ -44,19 +44,13 @@ num_uniq_zip <- uniqueN(dt[,zip])
 
 ## create bootstrap data for ReAd ----
 #' Save the bootstrapped data to accelerate computing
-dir.create(file.path("/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/data/bootstrap/ReAd/"), showWarnings = FALSE)
+dir.create(file.path(paste0(getwd(),"/data/bootstrap/ReAd/")), showWarnings = FALSE)
 
-lapply(1:200, function(boots_id){
+lapply(1:100, function(boots_id){
   set.seed(boots_id)
   zip_sample <- sample(1:num_uniq_zip, floor((num_uniq_zip)^0.7), replace=T) 
   dt_boots <- subset(dt, zip %in% all_zip[zip_sample]) 
-  write_fst(dt_boots, paste0("/nfs/home/S/shd968/shared_space/ci3_shd968/dementia/data/bootstrap/ReAd/", boots_id,".fst"))
-  cat("finish creating data", boots_id, "of 200\n")
+  write_fst(dt_boots, paste0(getwd(),"/data/bootstrap/ReAd/", boots_id,".fst"))
+  cat("finish creating data", boots_id, "of 100\n")
 })
-
-
-temp <- coxph(Surv(time=year_prev, time2 = year, event = dead)~as.factor(sex) + strata(as.factor(sex)),
-              data=dt,
-              ties = "efron")
-summary(temp)
 
